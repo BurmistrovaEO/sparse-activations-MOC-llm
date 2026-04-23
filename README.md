d238093ac23f32cf7e3b4c7b0811732fdf5fd4d5
Kate Burmistrova <kburhse@gmail.com>
2026-04-23 23:41:17 +0300

    added tapify

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+To stabilize the pylint behaviour during the development process, it is recommended to set *python.defaultInterpreterPath* variable in .vscode/settings.json to python in your virtual environment.
+
+___
 Скорее всего, финальный отчет по проекту нужно будет оформить в отдельный документ. На данный момент все изменения вносятся в мидтермовский и отмечаются розовым хайлайтером.
--- model_from_hf.py
+++ model_from_hf.py
@@ -1,20 +1,32 @@
 import os
 import torch
 import transformers
+from path import Path
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.models.llama.modeling_llama import LlamaMLP
 from huggingface_hub import login
 from lm_eval import evaluator, tasks
 from lm_eval.models.huggingface import HFLM
+from tap import Tap
 
 from sparsificaiton import SparseMLP, replace_nested_module
 
-HF_TOKEN = os.environ.get('HF_TOKEN')
+#HF_TOKEN = os.environ.get('HF_TOKEN')
 
-if __name__ == "__main__":
-    login(token=HF_TOKEN)
-    print(HF_TOKEN)
-    model_path = "meta-llama/Llama-3.2-3B"
+class ARGUMENT_PARSER(Tap):
+    config_path: Path = None
+    HF_TOKEN: str = None
+    model_path: str = "/Users/kateburmr/.cache/huggingface/hub/models--meta-llama--Llama-3.2-3B/snapshots/13afe5124825b4f3751f836b40dafda64c1ed062"
+
+#TODO rewrite as main
+def main(parsed_arguments):
+    #TODO check if HF_TOken is not none
+    if parsed_arguments.HF_TOKEN:
+        login(token=parsed_arguments.HF_TOKEN)
+        print(parsed_arguments.HF_TOKEN)
+        model_path = "meta-llama/Llama-3.2-3B"
+    else:
+        model_path = parsed_arguments.model_path
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForCausalLM.from_pretrained(model_path)
 
@@ -70,3 +82,8 @@ if __name__ == "__main__":
 
     # Print accuracy
     print(f"Accuracy: {results['results']['arc_easy']}")
+
+
+if __name__ == "__main__":
+    args = ARGUMENT_PARSER(underscores_to_dashes=True).parse_args()
+    main(args)
