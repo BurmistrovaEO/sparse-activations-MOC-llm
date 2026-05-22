f63926fe9c1c0991e3d8ef54ec073ccfc7a02dab
Kate Burmistrova <kburhse@gmail.com>
2026-05-22 19:55:14 +0300

    fixed flake8 in some files

--- ablations.py
+++ ablations.py
@@ -1,20 +1,30 @@
-from sparsity.sparsification import SparseMLP, NMsparseMLP, replace_nested_module
+from sparsity.sparsification import (
+                                SparseMLP,
+                                NMsparseMLP,
+                                replace_nested_module
+                                )
 from sparsity.optimized_forward_mac import SparseMLP as SParseFWMLP
 from transformers.models.llama.modeling_llama import LlamaMLP
 from sparsity.triton_sparsity import TritonSparseMLP
 import math
 
-IMPORTANCE_RANKING = [27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 15, 17, 16, 14, 13, 12, 11, 10, 9, 7, 8, 6, 5, 4, 3, 1, 2, 0]
+IMPORTANCE_RANKING = [27, 26, 25, 24, 23,
+                      22, 21, 20, 19, 18,
+                      15, 17, 16, 14, 13,
+                      12, 11, 10, 9, 7, 8,
+                      6, 5, 4, 3, 1, 2, 0]
 BEGIN = [0, 1, 2, 3, 4, 5]
 END = [22, 23, 24, 25, 26, 27]
 MIDDLE = [11, 12, 13, 14, 15, 16]
 BEGEND = [0, 1, 2, 25, 26, 27]
 
-def set_up_sparsification(model, ablation_kind, importance_percentage,
-                            sparse_implementation, device, k, n, m):
+
+def set_up_sparsification(model, ablation_kind, importance_perc,
+                          sparse_implementation, device, k, n, m):
     if ablation_kind == "importance":
-        assert 0 <= importance_percentage <= 100
-        selected_amount = math.ceil((len(IMPORTANCE_RANKING) * importance_percentage) / 100)
+        assert 0 <= importance_perc <= 100
+        selected_amount = (len(IMPORTANCE_RANKING) * importance_perc) / 100
+        selected_amount = math.ceil(selected_amount)
         sparse_indexes = IMPORTANCE_RANKING[:selected_amount]
     elif ablation_kind == "begin":
         sparse_indexes = BEGIN
@@ -33,7 +43,6 @@ def set_up_sparsification(model, ablation_kind, importance_percentage,
         sparse_class = SParseFWMLP
     else:
         sparse_class = TritonSparseMLP
-        
 
     to_replace_names_modules = {}
 
@@ -43,7 +52,7 @@ def set_up_sparsification(model, ablation_kind, importance_percentage,
             if counter in sparse_indexes:
                 to_replace_names_modules[name] = module
             counter += 1
-            
+
     for name, module in to_replace_names_modules.items():
         sparseBlock = sparse_class(module, k=k, n=n, m=m)
         replace_nested_module(model, name, sparseBlock)
--- argument_parser.py
+++ argument_parser.py
@@ -3,40 +3,75 @@ from pathlib import Path
 from typing import List
 import json
 
+
 class ARGUMENT_PARSER(Tap):
     config_path: Path = None
-    device: str = "mps:0" #["mps:0", "cuda:0", "cpu"] TODO: reimplement as enum
+    #  ["mps:0", "cuda:0", "cpu"] TODO: reimplement as enum
+    device: str = "mps:0"
     HF_TOKEN: str = None
-    model_path: str = "/Users/kateburmr/.cache/huggingface/hub/models--meta-llama--Llama-3.2-3B/snapshots/13afe5124825b4f3751f836b40dafda64c1ed062"
+    model_path: str = (
+                    "/Users/kateburmr/.cache/huggingface/hub/"
+                    "models--meta-llama--Llama-3.2-3B/snapshots/"
+                    "13afe5124825b4f3751f836b40dafda64c1ed062"
+    )
     lora_finetune: bool = False
     sparsify: bool = False
-    ablation_kind: str = "begin" #["begin", "end", "begend", "middle", "importance"] TODO: reimplement as enum
+    #  ["begin", "end", "begend", "middle", "importance"]
+    # TODO: reimplement as enum
+    ablation_kind: str = "begin"
     importance_percentage: int = None
-    sparse_implementation: str = "vanilla_k" #TODO: reimplement as enum
+    #  TODO: reimplement as enum
+    sparse_implementation: str = "vanilla_k"
     k_param: int = 4096
     n_param: int = 2
     m_param: int = 8
 
-    sparse_arg_names = ["ablation_kind", "importance_percentage", "sparse_implementation", "k_param", "n_param", "m_param"]
+    sparse_arg_names = [
+                "ablation_kind",
+                "importance_percentage",
+                "sparse_implementation",
+                "k_param",
+                "n_param",
+                "m_param"
+    ]
 
     dataset: str = "tatsu-lab/alpaca"
-    lora_rank: int = 2 # 8
-    lora_alpha: int = 4 # 16
+    lora_rank: int = 2  # 8
+    lora_alpha: int = 4  # 16
     lora_dropout: float = 0.05
-    target_modules: List[str] = ['gate_proj', 'up_proj', 'down_proj', 'k_proj', 'v_proj', 'o_proj', 'q_proj']
+    target_modules: List[str] = [
+                'gate_proj',
+                'up_proj',
+                'down_proj',
+                'k_proj',
+                'v_proj',
+                'o_proj',
+                'q_proj'
+    ]
 
     train_output_dir: str = "results/res0/mt0-large-lora"
     learning_rate: float = 1e-3
-    per_device_train_batch_size: int = 2 # 32
-    per_device_eval_batch_size: int = 2 # 32
+    per_device_train_batch_size: int = 2  # 32
+    per_device_eval_batch_size: int = 2  # 32
     num_train_epochs: int = 2
     weight_decay: float = 0.01
 
-    ft_arg_names = ["dataset", "lora_rank", "lora_alpha", "lora_dropout", "target_modules", \
-                    "train_output_dir", "learning_rate", "per_device_train_batch_size", \
-                    "per_device_eval_batch_size", "num_train_epochs", "weight_decay"]
+    ft_arg_names: List[str] = [
+        "dataset", "lora_rank", "lora_alpha",
+        "lora_dropout", "target_modules",
+        "train_output_dir", "learning_rate",
+        "per_device_train_batch_size",
+        "per_device_eval_batch_size",
+        "num_train_epochs", "weight_decay"
+    ]
 
-    hf_tasks: List[str] = ["hellaswag", "arc_challenge", "arc_easy", "boolq", "winogrande"]
+    hf_tasks: List[str] = [
+                "hellaswag",
+                "arc_challenge",
+                "arc_easy",
+                "boolq",
+                "winogrande"
+    ]
 
 
 def parse_and_join_config(parsed_arguments):
